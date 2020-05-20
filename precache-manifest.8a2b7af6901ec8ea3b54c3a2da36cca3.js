87fc99756c538a94cc1817df259a4f8ec9953840
santiagogrr <santiagog94@gmail.com>
2020-05-20 11:25:04 -0500

    Updates

---
+++ precache-manifest.8a2b7af6901ec8ea3b54c3a2da36cca3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e8a5701219b54c68a2091e17b1a1dfb",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "1d36f013e00792164fab",
+    "url": "/personal-portfolio/static/css/2.5206c547.chunk.css"
+  },
+  {
+    "revision": "403576fba63a2f463634",
+    "url": "/personal-portfolio/static/css/main.33a52a83.chunk.css"
+  },
+  {
+    "revision": "1d36f013e00792164fab",
+    "url": "/personal-portfolio/static/js/2.35e32730.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/personal-portfolio/static/js/2.35e32730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "403576fba63a2f463634",
+    "url": "/personal-portfolio/static/js/main.4931e180.chunk.js"
+  },
+  {
+    "revision": "e82e20abe3f05761aa4c",
+    "url": "/personal-portfolio/static/js/runtime-main.30ef1168.js"
+  },
+  {
+    "revision": "865e193452af48747be6fb896ec0a677",
+    "url": "/personal-portfolio/static/media/New_CV.865e1934.pdf"
+  },
+  {
+    "revision": "658b2e105b68fa396a315a58d4624081",
+    "url": "/personal-portfolio/static/media/Pshop_1139.658b2e10.png"
+  },
+  {
+    "revision": "a7845c97cc5cd4344ae8e55af8d71f84",
+    "url": "/personal-portfolio/static/media/photo1.a7845c97.png"
+  }
+]);
