3f24d2275ca7f2fb96244a1806fd490f3e2e431e
santiagogrr <santiagog94@gmail.com>
2020-06-04 12:35:19 -0500

    added projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -17,53 +17,53 @@ class Projects extends Component {
         <div className="projects-grid">
           <Grid>
           {/* Project 1 */}
-          <Cell phone={12}>    
-        <Card shadow={5}>
-          <CardTitle style={{color:'#fff', height: '176px', background: 'url(https://i.pinimg.com/originals/5c/7b/53/5c7b53a7be1dd267f24f7559584d1345.jpg) center / cover'}}>
-            Task/Reward App
-          </CardTitle>
-          <CardText>
-          Aimed to parents and kids with intellectual disabilities. This app is designed to be used by both the parent and the child at the same time. In this app, parents assign tasks for their children. When the child completes they receive points which can be redeemed for rewards.
-          </CardText>
-          <CardActions border>
-            <a href="https://github.com/santiagogrr/effeta-app/" target="_blank" rel="noopener noreferrer">
-              <Button colored>Github</Button>
-            </a>
-          </CardActions>
-        </Card>
-        </Cell>  
+          <Cell col={4} phone={12}>    
+            <Card shadow={5}>
+              <CardTitle style={{color:'#fff', height: '176px', background: 'url(https://i.pinimg.com/originals/5c/7b/53/5c7b53a7be1dd267f24f7559584d1345.jpg) center / cover'}}>
+                Task/Reward App
+              </CardTitle>
+              <CardText>
+              Aimed to parents and kids with intellectual disabilities. This app is designed to be used by both the parent and the child at the same time. In this app, parents assign tasks for their children. When the child completes they receive points which can be redeemed for rewards.
+              </CardText>
+              <CardActions border>
+                <a href="https://github.com/santiagogrr/effeta-app/" target="_blank" rel="noopener noreferrer">
+                  <Button colored>Github</Button>
+                </a>
+              </CardActions>
+            </Card>
+          </Cell>  
 
-        <Cell phone={12}>
-        <Card shadow={5}>
-          <CardTitle style={{color:'#fff', height: '176px', background: 'url(https://i.pinimg.com/originals/5c/7b/53/5c7b53a7be1dd267f24f7559584d1345.jpg) center / cover'}}>
-            Search Up App
-          </CardTitle>
-          <CardText>
-          This app allows students and collaborators to search teachers and physical spaces of my university (University of Monterrey). This was made with the purpose of solving the problem at the university of lack of signals within the campus.
-          </CardText>
-          <CardActions border>
-            <a href="https://github.com/santiagogrr/projectOne/" target="_blank" rel="noopener noreferrer">
-              <Button colored>Github</Button>
-            </a>
-          </CardActions>
-        </Card>
-        </Cell>
+          <Cell col={4} phone={12}>
+            <Card shadow={5}>
+              <CardTitle style={{color:'#fff', height: '176px', background: 'url(https://i.pinimg.com/originals/5c/7b/53/5c7b53a7be1dd267f24f7559584d1345.jpg) center / cover'}}>
+                Search Up App
+              </CardTitle>
+              <CardText>
+              This app allows students and collaborators to search teachers and physical spaces of my university (University of Monterrey). This was made with the purpose of solving the problem at the university of lack of signals within the campus.
+              </CardText>
+              <CardActions border>
+                <a href="https://github.com/santiagogrr/projectOne/" target="_blank" rel="noopener noreferrer">
+                  <Button colored>Github</Button>
+                </a>
+              </CardActions>
+            </Card>
+          </Cell>
 
-        <Cell phone={12}>
-        <Card shadow={5} >
-          <CardTitle style={{color:'#fff', height: '176px', background: 'url(https://i.pinimg.com/originals/5c/7b/53/5c7b53a7be1dd267f24f7559584d1345.jpg) center / cover'}}>
-            Memory Game
-          </CardTitle>
-          <CardText>
-          It's a memory game made with React Native. There are three levels of difficulty. The player has to find the matching country flag in the grid of cards. 
-          </CardText>
-          <CardActions border>
-            <a href="https://github.com/santiagogrr/memorama/" target="_blank" rel="noopener noreferrer">
-              <Button colored>Github</Button>
-            </a>
-          </CardActions>
-        </Card>
-        </Cell>
+          <Cell col={4} phone={12}>
+            <Card shadow={5} >
+              <CardTitle style={{color:'#fff', height: '176px', background: 'url(https://i.pinimg.com/originals/5c/7b/53/5c7b53a7be1dd267f24f7559584d1345.jpg) center / cover'}}>
+                Memory Game
+              </CardTitle>
+              <CardText>
+              It's a memory game made with React Native. There are three levels of difficulty. The player has to find the matching country flag in the grid of cards. 
+              </CardText>
+              <CardActions border>
+                <a href="https://github.com/santiagogrr/memorama/" target="_blank" rel="noopener noreferrer">
+                  <Button colored>Github</Button>
+                </a>
+              </CardActions>
+            </Card>
+          </Cell>
         </Grid>
         
 
@@ -91,20 +91,46 @@ class Projects extends Component {
     } else if(this.state.activeTab === 2){
       return(
         <div className="projects-grid">
-          {/* Project 1 */}         
-          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color:'#fff', height: '176px', background: 'url(https://i.pinimg.com/originals/5c/7b/53/5c7b53a7be1dd267f24f7559584d1345.jpg) center / cover'}}>
-              Covid-19 Tracker
-            </CardTitle>
-            <CardText>
-            Coronavirus tracker that shows the numbers and trends of people affected, recovered and deceased due to COVID-19 worldwide and by country.
-            </CardText>
-            <CardActions border>
-            <a href="https://github.com/santiagogrr/COVID-tracker" target="_blank" rel="noopener noreferrer">
-              <Button colored>Github</Button>
-            </a>
-            </CardActions>
-          </Card>
+          {/* Project 1 */}   
+          <Grid>
+            
+            <Cell col={6} phone={12}>
+              <Card shadow={5}>
+                <CardTitle style={{color:'#fff', height: '176px', background: 'url(https://i.pinimg.com/originals/5c/7b/53/5c7b53a7be1dd267f24f7559584d1345.jpg) center / cover'}}>
+                  Covid-19 Global Tracker
+                </CardTitle>
+                <CardText>
+                Coronavirus tracker that shows the numbers and trends of people affected, recovered and deceased due to COVID-19 worldwide and by country.
+                </CardText>
+                <CardActions border>
+                <a href="https://github.com/santiagogrr/COVID-tracker" target="_blank" rel="noopener noreferrer">
+                  <Button colored>Github</Button>
+                </a>
+                </CardActions>
+              </Card>
+            </Cell>  
+            
+            <Cell col={6} phone={12}>
+              <Card shadow={5}>
+                <CardTitle style={{color:'#fff', height: '176px', background: 'url(https://i.pinimg.com/originals/5c/7b/53/5c7b53a7be1dd267f24f7559584d1345.jpg) center / cover'}}>
+                  Covid-19 Mexico Tracker
+                </CardTitle>
+                <CardText>
+                Coronavirus tracker that shows the numbers and graphs of people infected, recovered and deceased due to COVID-19 in Mexico.
+                </CardText>
+                <CardActions border>
+                <a href="https://github.com/santiagogrr/covid-mexico" target="_blank" rel="noopener noreferrer">
+                  <Button colored>Github</Button>
+                </a>
+                <a href="https://covid-19-mexico-tracker.netlify.app/" target="_blank" rel="noopener noreferrer">
+                  <Button colored>Demo</Button>
+                </a>
+                </CardActions>
+              </Card>
+            </Cell>  
+ 
+          
+          </Grid>  
         </div>
       )
     }
@@ -114,16 +140,18 @@ class Projects extends Component {
   render(){
     return (
       <div className="contact-body">
-          <Row>
-            <Col align='center'>
-                <h1>Projects</h1>
-            </Col>
-          </Row>
+        <Row>
+          <Col align='center'>
+              <h1>Projects</h1>
+          </Col>
+        </Row>
+        <Grid>
         <Tabs activeTab ={this.state.activeTab} onChange={tabId => this.setState({activeTab:tabId})} ripple >
           <Tab>React Native</Tab>
           <Tab>Machine Learning Research</Tab>
           <Tab>React</Tab>
         </Tabs>
+        </Grid>
         
           <Grid>
             <Cell col={12}>
